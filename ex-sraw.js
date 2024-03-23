94458196482baf78ebeba37d0a910384dc4c33d7
Blugray <162154088+BlugrayGuyLegit@users.noreply.github.com>
2024-03-23 11:15:29 +0100

    Create ex-sraw.js

---
+++ ex-sraw.js
@@ -0,0 +1,20 @@
+// Hey you, if you looking for a secret code, there is nothing here ok! :)
+
+function warndel() {
+  console.log('Info-alert executed');
+  console.warn('click the button right-bottom');
+  console.warn('tip console clear to clear this.')
+  console.info({InfoName : 'Deleted things', Where : 'Beside'})
+  console.info(['Skibidi Toilet Ties', 'Ties from embracing', 'Dark tie', 'Special Blugray']);
+  var Deltie;
+  console.info({Name : 'DelTie', Type : 'Variable', Content : 'ST Tie / Dark Tie / Embracing Tie / Special Blugray', Value : '4'});
+  alert('⚠️ Warning:\nThe section with all of ties was deleted and the logo of the website with due to some image bug.')
+}
+
+var openPage = 1;
+var openPageFalse = 2;
+document.addEventListener('DOMContentLoaded', function() {
+  console.info('open')
+  console.info({Name : 'Page open', Value : openPage, Info : 'If value = 1 = Page successful opened'})
+  console.info({Name : 'Test (Page not open)', Value : openPageFalse, So : '2 = False'})
+});
